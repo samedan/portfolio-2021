5c3a7beb5f28d4fa5a542347b2d8676959829445
Daniel Popescu <same.dan@gmail.com>
2021-01-17 18:49:25 +0100

    Finished Weekend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/main.scss";
+import "@/styles/main.scss";
+import "slate-simple-editor/dist/index.css";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -1,13 +1,22 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
-
+import { Editor } from "slate-simple-editor";
 import withAuth from "@/hoc/withAuth";
 
 const BlogEditor = ({ user, loading }) => {
+  const saveBlog = () => {
+    console.log(data);
+  };
   return (
     <BaseLayout user={user} loading={loading}>
-      <BasePage header="BlogEditor">
-        <h1>BlogEditor</h1>
+      <BasePage>
+        <Editor
+          // header="Ypour own title"
+          // loading={true}
+          onSave={(data) => {
+            console.log(data);
+          }}
+        />
       </BasePage>
     </BaseLayout>
   );
